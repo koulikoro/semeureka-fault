eee8c56ef884db620710222116dbb1a1eef16786
FORLUV <FORLUV@FORLUV-PC>
2014-06-02 16:13:05 +0800

    update minaService.
    Signed-off-by: FORLUV <FORLUV@FORLUV-PC>

---
+++ com.semeureka.fault/src/main/java/com/semeureka/fault/comm/CommSupport.java
@@ -0,0 +1,190 @@
+package com.semeureka.fault.comm;
+
+import gnu.io.CommPortIdentifier;
+
+import java.io.IOException;
+import java.net.SocketAddress;
+import java.rmi.ConnectException;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.apache.mina.core.future.ConnectFuture;
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.core.service.IoConnector;
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.transport.serial.SerialConnector;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+import org.apache.mina.transport.socket.nio.NioSocketConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@DependsOn("libraryPathRegister")
+public class CommSupport {
+	public static final Logger logger = LoggerFactory.getLogger(CommSupport.class);
+	private IoAcceptor server = new NioSocketAcceptor();
+	private IoConnector client = new NioSocketConnector();
+	private IoConnector serial = new SerialConnector();
+	private boolean serverStart;
+	private boolean clientStart;
+	private boolean serialStart;
+
+	@Value("${fault.server.address}")
+	public void setServerAddress(SocketAddress serverAddress) {
+		if (!server.isActive()) {
+			this.server.setDefaultLocalAddress(serverAddress);
+		}
+	}
+
+	@Value("${fault.client.address}")
+	public void setClientAddress(SocketAddress clientAddress) {
+		this.client.setDefaultRemoteAddress(clientAddress);
+	}
+
+	@Value("${fault.serial.address}")
+	public void setSerialAddress(SocketAddress serialAddress) {
+		this.serial.setDefaultRemoteAddress(serialAddress);
+	}
+
+	public boolean isServerStart() {
+		return serverStart;
+	}
+
+	@Value("${fault.server.start}")
+	public void setServerStart(boolean serverStart) {
+		this.serverStart = serverStart;
+	}
+
+	public boolean isClientStart() {
+		return clientStart;
+	}
+
+	@Value("${fault.client.start}")
+	public void setClientStart(boolean clientStart) {
+		this.clientStart = clientStart;
+	}
+
+	public boolean isSerialStart() {
+		return serialStart;
+	}
+
+	@Value("${fault.serial.start}")
+	public void setSerialStart(boolean serialStart) {
+		this.serialStart = serialStart;
+	}
+
+	public IoAcceptor getServer() {
+		return server;
+	}
+
+	public IoConnector getClient() {
+		return client;
+	}
+
+	public IoConnector getSerial() {
+		return serial;
+	}
+
+	@PostConstruct
+	public synchronized void toggleServer() throws IOException {
+		if (serverStart && !server.isActive()) {
+			server.setHandler(new IoHandlerAdapter()); // TODO
+			server.bind();
+			logger.info("Start server {} success", server.getDefaultLocalAddress());
+		} else if (server.isActive()) {
+			server.unbind();
+			logger.info("Stop server {} success", server.getDefaultLocalAddress());
+		}
+	}
+
+	@PostConstruct
+	public synchronized void toggleClient() throws IOException {
+		if (clientStart && !client.isActive()) {
+			client.setHandler(new IoHandlerAdapter()); // TODO
+			ConnectFuture connect = client.connect().awaitUninterruptibly();
+			if (!connect.isConnected()) {
+				throw new ConnectException("Connection timed out: connect");
+			}
+			logger.info("Start client {} success", client.getDefaultRemoteAddress());
+		} else if (client.isActive()) {
+			Iterator<IoSession> iterator = client.getManagedSessions().values().iterator();
+			while (iterator.hasNext()) {
+				iterator.next().close(true);
+			}
+			logger.info("Stop client {} success", client.getDefaultRemoteAddress());
+		}
+	}
+
+	@PostConstruct
+	public synchronized void toggleSerial() throws IOException {
+		if (serialStart && !serial.isActive()) {
+			serial.setHandler(new IoHandlerAdapter()); // TODO
+			ConnectFuture connect = serial.connect().awaitUninterruptibly();
+			if (!connect.isConnected()) {
+				throw new ConnectException("Connection timed out: connect");
+			}
+			logger.info("Start serial {} success", serial.getDefaultRemoteAddress());
+		} else if (serial.isActive()) {
+			Iterator<IoSession> iterator = serial.getManagedSessions().values().iterator();
+			while (iterator.hasNext()) {
+				iterator.next().close(true);
+			}
+			logger.info("Stop serial {} success", serial.getDefaultRemoteAddress());
+		}
+	}
+
+	@PreDestroy
+	public synchronized void stopAll() {
+		server.unbind();
+		Iterator<IoSession> iterator1 = client.getManagedSessions().values().iterator();
+		while (iterator1.hasNext()) {
+			iterator1.next().close(true);
+		}
+		Iterator<IoSession> iterator2 = serial.getManagedSessions().values().iterator();
+		while (iterator2.hasNext()) {
+			iterator2.next().close(true);
+		}
+	}
+
+	public List<CommPortIdentifier> getCommPorts() {
+		List<CommPortIdentifier> CommPorts = new LinkedList<CommPortIdentifier>();
+		Enumeration<?> portList = CommPortIdentifier.getPortIdentifiers();
+		while (portList.hasMoreElements()) {
+			CommPorts.add((CommPortIdentifier) portList.nextElement());
+		}
+		return CommPorts;
+	}
+
+	@Scheduled(initialDelay = 60000, fixedDelay = 60000)
+	public synchronized void checkAll() {
+		if (serverStart && !server.isActive()) {
+			try {
+				toggleServer();
+			} catch (IOException e) {
+				logger.error("restart server {} failed", server, e);
+			}
+		} else if (clientStart && !client.isActive()) {
+			try {
+				toggleClient();
+			} catch (IOException e) {
+				logger.error("restart client {} failed", server, e);
+			}
+		} else if (serialStart && !serial.isActive()) {
+			try {
+				toggleSerial();
+			} catch (IOException e) {
+				logger.error("restart serial {} failed", server, e);
+			}
+		}
+	}
+}
